1897cfd38f4df65ac57651addd1ce113cde3894b
Vesela Yanyova <147051362+Vesi793@users.noreply.github.com>
2025-04-20 15:57:23 +0100

    Add folder: FindTheBiggNumber

---
+++ findTheBiggNumber/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace findTheBiggNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first number: ");
+            int a = int.Parse(Console.ReadLine());
+          
+            Console.Write("Enter the second number: ");
+            int b = int.Parse(Console.ReadLine());
+            
+            Console.Write("Enter the third number: ");
+            int c = int.Parse(Console.ReadLine());
+
+            if (a >= b && a >= c)
+            {
+                Console.WriteLine($"The biggest number is: {a}");
+            }
+        else if (b >= a && b >= c)
+            {
+                Console.WriteLine($"The biggest number is: {b}");
+            }
+            else
+            {
+                Console.WriteLine($"The biggest number is: {c}");
+            }
+        }
+    }
+}
