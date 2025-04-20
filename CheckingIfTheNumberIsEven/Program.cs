0f43a430228657150f869d78295c980e617c7a7c
Vesela Yanyova <147051362+Vesi793@users.noreply.github.com>
2025-04-20 15:46:26 +0100

    Add folder: CheckingIfTheNumberIsEven

---
+++ CheckingIfTheNumberIsEven/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace CheckingIfTheNumberIsEven
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number:");
+            int n = int.Parse(Console.ReadLine());
+
+            if (n % 2 == 0)
+            {
+                Console.WriteLine("The number is even.");
+            }
+            else
+            {
+                Console.WriteLine("The number is odd.");
+            }
+        }
+    }
+}
