0c7ab6636a74c7d899c1ba95183581c50244b37f
Vesela Yanyova <147051362+Vesi793@users.noreply.github.com>
2025-04-20 15:56:24 +0100

    Add folder: EvenOddNumber

---
+++ evenOddNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace evenOddNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine($"The number is even:  { number}");
+            }
+            else
+            {
+                Console.WriteLine($"The number is odd:  {number}");
+            }
+
+
+        }
+    }
+}
