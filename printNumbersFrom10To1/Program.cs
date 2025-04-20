17e64c5fbbe49e383421196c7bffc0ec964d0fde
Vesela Yanyova <147051362+Vesi793@users.noreply.github.com>
2025-04-20 15:50:16 +0100

    Add folder: PrintNumbersFrom10To1

---
+++ printNumbersFrom10To1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace printNumbersFrom10To1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 10; i >= 1; i--)
+            {
+Console.WriteLine(i);
+            }
+        }
+    }
+}
