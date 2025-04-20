c74468321e5e921c31abf809e662a5635109771f
Vesela Yanyova <147051362+Vesi793@users.noreply.github.com>
2025-04-20 15:59:44 +0100

    Add folder: PositiveNegativeOrNull2

---
+++ positiveNegativeOrNull2/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace positiveNegativeOrNull
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number: ");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num == 0)
+            {
+                Console.WriteLine($"The number is zero.");
+            }
+            else if (num > 0)
+            {
+                Console.WriteLine($"The number is positive: {num}");
+            }
+            else
+            {
+                Console.WriteLine($"The number is negative: {num}");
+            }
+        }
+    }
+}
