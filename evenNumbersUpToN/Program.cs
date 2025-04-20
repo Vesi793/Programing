0cc622b1f264741e96eba525224e2387d626bf19
Vesela Yanyova <147051362+Vesi793@users.noreply.github.com>
2025-04-20 15:47:42 +0100

    add folder: EvenNumbersUpToN

---
+++ evenNumbersUpToN/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace evenNumbersUpToN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= n; i++)
+            if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+        }
+    }
+}
