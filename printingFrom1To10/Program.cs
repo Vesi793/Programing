5515362572345bef89374625255e7f28d0830693
Vesela Yanyova <147051362+Vesi793@users.noreply.github.com>
2025-04-20 15:49:40 +0100

    Add folder: PrintingFrom1To10

---
+++ printingFrom1To10/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace printingFrom1To10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
