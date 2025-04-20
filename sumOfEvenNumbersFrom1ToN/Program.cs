829f88d1d962177225e3a6bef2b140961fd9fab1
Vesela Yanyova <147051362+Vesi793@users.noreply.github.com>
2025-04-20 15:50:49 +0100

    Add folder: SumOfEvenNumbersFrom1ToN

---
+++ sumOfEvenNumbersFrom1ToN/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace sumOfEvenNumbersFrom1ToN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sum += i;
+                }
+            }
+                Console.WriteLine($"The sum is: {sum}");
+            
+        }
+    }
+}
