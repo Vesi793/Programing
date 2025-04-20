70dd26aa5f656aef88f6e33aad09c5f9ff6f8d0f
Vesela Yanyova <147051362+Vesi793@users.noreply.github.com>
2025-04-20 15:48:25 +0100

    add folder: FindTheLargerOfTheTwoNumbers

---
+++ findTheLargerOfTheTwoNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace findTheLargerOfTheTwoNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first number: ");
+            int n1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the second number: ");
+            int n2 = int.Parse(Console.ReadLine());
+
+            if (n1 > n2)
+            {
+                Console.WriteLine("The first number is bigger.");
+            }
+            else if (n2 > n1)
+            {
+                Console.WriteLine("The second number is bigger.");
+            }
+            else
+            {
+                Console.WriteLine("The numbers are equal.");
+            }
+        }
+    }
+}
