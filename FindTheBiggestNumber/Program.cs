8e4e3ed3a66341f1ccae1ce6cd2e5b23d28e85ec
Vesela Yanyova <147051362+Vesi793@users.noreply.github.com>
2025-04-20 15:56:50 +0100

    Add folder: FindTheBiggestNumber

---
+++ FindTheBiggestNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace FindTheBiggestNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number!");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number!");
+            int num2 = int.Parse(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                Console.WriteLine($"The bigger number is: {num1}");
+            }
+            else if (num1 > num2)
+            {
+                Console.WriteLine($"The bigger number is: {num2}");
+            }
+            else 
+                    {
+                Console.WriteLine($"The numbers are equal!");
+            }
+        }
+    }
+}
