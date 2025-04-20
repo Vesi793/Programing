011fc55ed3088dab5e475826aad86100d3aa30c1
Vesela Yanyova <147051362+Vesi793@users.noreply.github.com>
2025-04-20 15:57:53 +0100

    Add folder: FindTheSmallesNumber

---
+++ findTheSmallesNumber/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace findTheSmallesNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first number: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the second number: ");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the third number: ");
+            int c = int.Parse(Console.ReadLine());
+
+
+
+
+            if (a <= b && a <= c)
+            {
+                Console.WriteLine($"The smallest number is: {a}");
+            }
+            else if (b <= a && b <= c)
+            {
+                Console.WriteLine($"The smallest number is: {b}");
+            }
+            else
+            {
+                Console.WriteLine($"The smallest number is: {c}");
+            }   }
+    }
+}
